d0b3ef720aedca16869cc488b8d255841ca6a587
Alain Sst <luumanhquan.91@gmail.com>
2022-04-23 05:04:21 -0400

    feat(update code) update code

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,20 @@
 package app
+
+import (
+
+)
+
+type App interface {
+
+}
+
+type app struct {
+
+}
+
+func NewApp() App {
+	return &app{
+
+	}
+}
+
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,22 @@
 package server
+
+import (
+
+)
+
+type Server interface {
+	AppServerAPI() error
+}
+
+type server struct {
+
+}
+
+func NewSserver() Server {
+	return &server{}
+}
+
+func (s *server) AppServerAPI() error {
+	
+	return nil 
+}
